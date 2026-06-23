80a78d0457a6501c1a202c28c97a6741ce004c58
Pranay Kalekar <pranaykalekar20@gmail.com>
2026-06-23 14:07:59 +0530

    Update

--- travel-frontend/app/layout.tsx
+++ travel-frontend/app/layout.tsx
@@ -1,32 +1,32 @@
-// import "./globals.css";
-// import Navbar from "@/components/navbar"; // Path check kar
+import "./globals.css";
+import Navbar from "@/components/navbar"; // Path check kar
 
 
-// import Footer from "@/components/footer";
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <Navbar />
-//         <main>{children}</main>
-//         <Footer /> {/* २. इथे फुटर ॲड करा */}
-//       </body>
-//     </html>
-//   );
-// }
-
-
-import Providers from "./Providers"; // आपण बनवलेली फाईल इथे इंपोर्ट कर
+import Footer from "@/components/footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Navbar />
+        <main>{children}</main>
+        <Footer /> {/* २. इथे फुटर ॲड करा */}
       </body>
     </html>
   );
 }
+
+
+// import Providers from "./Providers"; // आपण बनवलेली फाईल इथे इंपोर्ट कर
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <Providers>
+//           {children}
+//         </Providers>
+//       </body>
+//     </html>
+//   );
+// }
