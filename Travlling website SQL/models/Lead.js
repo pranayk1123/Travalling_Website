56ecbe0a37c984fe07189433c66efa72c1e92cbd
Pranay Kalekar <pranaykalekar20@gmail.com>
2026-06-25 00:46:47 +0530

    Update

--- Travlling website SQL/models/Lead.js
+++ Travlling website SQL/models/Lead.js
@@ -15,6 +15,13 @@ const Lead = sequelize.define("Lead", {
   type: {
     type: DataTypes.STRING,
     defaultValue: "contact" // जर काहीच नाही पाठवलं तर तो 'contact' म्हणून सेव्ह होईल
+  },
+  
+  // 🚀 नवीन: Password Request Status के लिए
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: "pending",
+    allowNull: true
   }
 });
 
